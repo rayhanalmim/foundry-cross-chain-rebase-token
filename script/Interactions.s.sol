2a8f916d3b8217d2d82a5b08d758c7c9e975d03d
rayhanalmim <rayhanalmim1@gmail.com>
2025-02-09 12:07:01 +0600

    fixed file import path

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Script} from "forge-std/Script.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
 import {Vault} from "../src/Vault.sol";
 
 contract DepositScript is Script {
